c8e4af0b4c27706348aefea01f6f3c627d746999
kingarthurdev <140220795+kingarthurdev@users.noreply.github.com>
2026-02-06 02:23:57 -0600

    ughhhh - more updates, but there are still a ton of bugs :(

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct testingReturnType
 } testingReturnType;
 
 // prior defs:
-testingReturnType processLine(char *lineInput, int outputRun);
+testingReturnType processLine(char *lineInput);
 void processInputFile(char *inputFilePath);
 void throwError(char *message);
 void processInstructions(testingReturnType *input, char *lineInput);
@@ -30,6 +30,9 @@ char *convertInMacro(char *input);
 char *convertOutMacro(char *input);
 char *convertPushMacro(char *input);
 char *convertPopMacro(char *input);
+char *parseAndFormatArgs(char *inputString);
+void autoAdd(char *inputString);
+void autoAddMacro(char *inputString);
 
 PDArrayList listOfLabels;
 const char delimiters[] = ", ";
@@ -37,6 +40,8 @@ const char delimiters[] = ", ";
 // initialize some "string builders" and hope 5k chars is enough
 char code[5000] = {0};
 char data[5000] = {0};
+
+char codeAndDataCombined[10000] = {0}; // used for final output
 
 // 0 for data mode, 1 for int, -1 for unset
 int type = -1;
@@ -47,7 +52,11 @@ int hasSeenAtLeast1Code = 0;
 // count # of prior instructions (starting at 0x1000 since that's where the program starts)
 long long memNumBytes = 0x1000;
 
-int numRuns = 0;
+int numRuns = 0; // 0 for the first run, 1 for the next (so we can preeval all the labels)
+
+int debuggingLineCount = 0;
+
+int isFirstLineInOutput = 1;
 
 int main(int argc, char *argv[])
 {
@@ -65,6 +74,7 @@ int main(int argc, char *argv[])
 	{
 		processInputFile(inputFilePath);
 		runTests();
+		printf("%s", codeAndDataCombined);
 	}
 
 	return 0;
@@ -80,41 +90,75 @@ void processInputFile(char *inputFilePath)
 		// first run
 		while (fgets(line, BUFFER_SIZE, file) != NULL)
 		{
-			processLine(line, 0);
+			processLine(line);
 		}
+
+		numRuns++;
+		code[0] = '\0';
+		data[0] = '\0';
+		codeAndDataCombined[0] = '\0';
+		isFirstLineInOutput = 1;
+		rewind(file);
+		type = -1;
+		memNumBytes = 0x1000;
 
 		// run again to actually write the data after the symbol table has been generated
 		while (fgets(line, BUFFER_SIZE, file) != NULL)
 		{
-			processLine(line, 1);
+			processLine(line);
 		}
+		printf("DONE! \n");
+		printf("%s\n", codeAndDataCombined);
 	}
 	else
 	{
 		throwError("ERROR! INVALID FILE INPUT PASSED (cannot access file)!");
 	}
 }
-//output run should be 0 if this is just the first pass, 1 if final passs
-testingReturnType processLine(char *lineInput, int outputRun)
+// output run should be 0 if this is just the first pass, 1 if final passs
+testingReturnType processLine(char *lineInput)
 {
 	testingReturnType tempThingy;
-	if ((lineInput[0]) == '\t') // should be instruction
+	if ((lineInput[0]) == '\t') // should be instruction or data
 	{
 		if (type == -1)
 		{
 			throwError("ERROR! DATA/INSTRUCTION TYPE UNDEFINED!");
 		}
-		processInstructions(&tempThingy, lineInput);
+		if (type == 1) // .code mode - process as instruction
+		{
+			processInstructions(&tempThingy, lineInput);
+		}
+		else if (type == 0) // .data mode - process as 64 bit data value
+		{
+			tempThingy.type = 0;
+			char *valuePart = lineInput + 1; // skip the tab
+			stripChars(valuePart, ' ');
+			stripChars(valuePart, '\n');
+
+			long long value = strtoll(valuePart, NULL, 0);
+
+			char dataLine[50];
+			sprintf(dataLine, "\n\t%lld", value);
+			sprintf(data + strlen(data), "%s", dataLine);
+
+			incrementBytes(type); // add 8 bytes for data
+			debuggingLineCount++;
+		}
 	}
 	else if ((lineInput[0]) == ':') // label
 	{
 		tempThingy.type = 2;
 		tempThingy.line = "";
-		PD newPD;
-		char buffer[7] = {0};
-		sprintf(buffer, "0x%x", memNumBytes);
-		newPD.replacementString = buffer;
-		addPD(&listOfLabels, &newPD);
+		PD *newPD = malloc(sizeof(PD));
+		newPD->replacementInt = memNumBytes;
+		stripChars(lineInput, ' ');
+		stripChars(lineInput, '\t');
+		stripChars(lineInput, '\n');
+
+		newPD->thingToReplace = strdup(lineInput);
+		if (numRuns == 0)
+			addPD(&listOfLabels, newPD);
 		// todo: add to symbol table (like hash table) and then do like a preprocessor directive cleanup at the very end once all the things have been resolved.
 		// lowk too lazy to make hashtable
 	}
@@ -125,6 +169,38 @@ testingReturnType processLine(char *lineInput, int outputRun)
 	}
 	else if ((lineInput[0]) == '.')
 	{ // either .code or .data
+		if (type != -1)
+		{
+			if (type == 1)
+			{
+				if (isFirstLineInOutput)
+				{
+					sprintf(codeAndDataCombined + strlen(codeAndDataCombined), ".code");
+					isFirstLineInOutput = 0;
+				}
+				else
+				{
+					sprintf(codeAndDataCombined + strlen(codeAndDataCombined), "\n.code");
+				}
+				sprintf(codeAndDataCombined + strlen(codeAndDataCombined), code);
+				code[0] = '\0';
+			}
+			else if (type == 0)
+			{
+				if (isFirstLineInOutput)
+				{
+					sprintf(codeAndDataCombined + strlen(codeAndDataCombined), ".data");
+					isFirstLineInOutput = 0;
+				}
+				else
+				{
+					sprintf(codeAndDataCombined + strlen(codeAndDataCombined), "\n.data");
+				}
+				sprintf(codeAndDataCombined + strlen(codeAndDataCombined), data);
+				data[0] = '\0';
+			}
+		}
+
 		if (strncmp(lineInput, ".data", 5) == 0)
 		{
 			tempThingy.type = 0;
@@ -141,6 +217,10 @@ testingReturnType processLine(char *lineInput, int outputRun)
 			throwError("ERROR! INVALID LINE STARTING WITH DOT!");
 		}
 	}
+	else if (lineInput[0] == '\n')
+	{
+		return tempThingy;
+	}
 	else
 	{
 		throwError("ERROR! UNRECOGNIZED LINE FORMAT!");
@@ -151,7 +231,7 @@ testingReturnType processLine(char *lineInput, int outputRun)
 
 void throwError(char *message)
 {
-	fprintf(stderr, "%s\n", message);
+	fprintf(stderr, "%s\n %i", message, debuggingLineCount);
 	exit(EXIT_FAILURE);
 }
 
@@ -162,46 +242,61 @@ void processInstructions(testingReturnType *input, char *lineInput)
 	if (startsWith2(lineInput, threeByThrees, 5))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
+		autoAdd(lineInput);
 	}
 	else if (startsWith2(lineInput, fourByTwo, 3))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 2);
-		// rd <- rd + L (Opcode 0x19)
+		autoAdd(lineInput);
+
+		// rd <- rd + L
 	}
 	else if (startsWith("call", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 4 + 1, 1);
-		// pc <- rd, saves return addr (Opcode 0xc)
+		// pc <- rd, saves return addr
+		autoAdd(lineInput);
 	}
 
 	else if (startsWith("or", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 2 + 1, 3);
-		// rd <- rs | rt (Opcode 0x1f)
+		// rd <- rs | rt
+		autoAdd(lineInput);
 	}
 	else if (startsWith("shftli", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 6 + 1, 2);
-		// rd <- rd << L (Opcode 0x22)
+		autoAdd(lineInput);
+		// rd <- rd << L
 	}
 	else if (startsWith("shftl", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 5 + 1, 3);
-		// rd <- rs << rt (Opcode 0x21)
+		autoAdd(lineInput);
+		// rd <- rs << rt
 	}
 	else if (startsWith("shftri", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 6 + 1, 2);
-		// rd <- rd >> L (Opcode 0x24)
+		autoAdd(lineInput);
+		// rd <- rd >> L
 	}
 	else if (startsWith("shftr", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 5 + 1, 3);
-		// rd <- rs >> rt (Opcode 0x23)
+		autoAdd(lineInput);
+		// rd <- rs >> rt
 	}
 	else if (startsWith("mov", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 2);
+		autoAdd(lineInput);
 		// Could be:
 		// 1. mov rd, rs (Reg-to-Reg)
 		// 2. mov rd, (rs)(L) (Load from memory)
@@ -209,93 +304,119 @@ void processInstructions(testingReturnType *input, char *lineInput)
 	}
 	else if (startsWith("brr", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 3 + 1, 1);
+		autoAdd(lineInput);
 		// NOTE: can be brr r_d OR brr L
-		//  Branch Relative: PC <- PC + L (Opcode 0x40)
+		//  Branch Relative: PC <- PC + L
 	}
 	else if (startsWith("brgt", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
-		// pc <- rd if rs > rt (Opcode 0xe)
+		autoAdd(lineInput);
+		// pc <- rd if rs > rt
 	}
 	else if (startsWith("br", lineInput))
 	{
+
 		handleValidateNumArgs(lineInput + 2 + 1, 1);
-		// pc <- rd (Opcode 0x8)
+		autoAdd(lineInput);
+		// pc <- rd
 	}
 	else if (startsWith("return", lineInput))
 	{
-		// No args (Opcode 0xd)
+		handleValidateNumArgs(lineInput + 6 + 1, 0);
+
+		autoAdd(lineInput);
+
+		// No args
 	}
 	else if (startsWith("addf", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
-		// Opcode 0x14
+		autoAdd(lineInput);
 	}
 	else if (startsWith("subf", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
-		// Opcode 0x15
+		autoAdd(lineInput);
 	}
 	else if (startsWith("mulf", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
-		// Opcode 0x16
+		autoAdd(lineInput);
 	}
 	else if (startsWith("divf", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 3);
-		// Opcode 0x17
+		autoAdd(lineInput);
 	}
 	else if (startsWith("and", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 3);
-		// rd <- rs & rt (Opcode 0x0)
+		autoAdd(lineInput);
+		// rd <- rs & rt
 	}
 	else if (startsWith("not", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 2);
-		// rd <- ~rs (Opcode 0x3)
+		autoAdd(lineInput);
+		// rd <- ~rs
 	}
 	else if (startsWith("priv", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 4);
-		// priv rd, rs, rt, L (Opcode 0xf)
+		autoAdd(lineInput);
+		// priv rd, rs, rt, L
 	}
 	// === MACROS (to be expanded later) ===
 	else if (startsWith("clr", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 1);
+		autoAddMacro(convertClrMacro(lineInput));
 		// Macro: expands to xor rd, rd, rd
 	}
 	else if (startsWith("ld", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 2 + 1, 2);
+		autoAddMacro(convertLdMacro(lineInput));
+
 		// Macro: expands to xor + addi/shftli chain
 	}
 	else if (startsWith("halt", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 0);
+		autoAddMacro(convertHaltMacro(lineInput));
+
 		// Macro: no args, expands to priv r0, r0, r0, 0x0
 	}
 	else if (startsWith("in", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 2 + 1, 2);
+		autoAddMacro(convertInMacro(lineInput));
+
 		// Macro: expands to priv rd, rs, r0, 0x3
 	}
 	else if (startsWith("out", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 2);
+		autoAddMacro(convertOutMacro(lineInput));
+
 		// Macro: expands to priv rd, rs, r0, 0x4
 	}
 	else if (startsWith("push", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 4 + 1, 1);
+		autoAddMacro(convertPushMacro(lineInput));
+
 		// Macro: push rd onto stack
 	}
 	else if (startsWith("pop", lineInput))
 	{
 		handleValidateNumArgs(lineInput + 3 + 1, 1);
+		autoAddMacro(convertPopMacro(lineInput));
+
 		// Macro: pop into rd from stack
 	}
 	else
@@ -304,7 +425,7 @@ void processInstructions(testingReturnType *input, char *lineInput)
 	}
 }
 
-//todo: implement
+// todo: implement
 void storePDs()
 {
 }
@@ -332,7 +453,10 @@ void stripChars(char *input, char charToStrip)
 // exists for testing purposes
 int validateNumArgs(char *inputs, int numExpectedArgs)
 {
-	char *token = strtok(inputs, delimiters);
+	char *copy = malloc(sizeof(char) * (strlen(inputs) + 1));
+	strcpy(copy, inputs);
+
+	char *token = strtok(copy, delimiters);
 
 	int count = 0;
 	while (1)
@@ -358,6 +482,7 @@ void handleValidateNumArgs(char *inputs, int numExpectedArgs)
 		throwError("ERROR! INVALID SYNTAX FOR INSTRUCTION PASSED!");
 	}
 	incrementBytes(type);
+	debuggingLineCount++;
 }
 
 // Return 1 if true, 0 if false, NOTE: extra +1 added to account for tab indent
@@ -395,7 +520,7 @@ long long evaluateLabel(char *label)
 	{
 		if (strcmp(label, listOfLabels.data[i]->thingToReplace) == 0)
 		{
-			return listOfLabels.data[i]-> replacementInt;
+			return listOfLabels.data[i]->replacementInt;
 		}
 	}
 	throwError("ERROR! LABEL NOT RECOGNIZED!");
@@ -407,102 +532,227 @@ void runTests()
 	printf("TESTING MODE ACTIVE!");
 }
 
-// halt -> priv r0, r0, r0, 0x0
+// halt
 char *convertHaltMacro(char *input)
 {
 	return "priv r0, r0, r0, 0x0";
 }
 
-// clr rd -> xor rd, rd, rd
+// clr rd
 char *convertClrMacro(char *input)
 {
-	char *buffer = malloc(sizeof(char)*10); // more than enough space
-	input += 4;			   // to get rid of the tab + clr
+	char *buffer = malloc(sizeof(char) * 30); // more than enough space
+	buffer[0] = '\0';
+	input += 4; // to get rid of the tab + clr
 	char *token = strtok(input, delimiters);
 	sprintf(buffer, "\n\txor %s, %s, %s", token, token, token);
 	return buffer;
 }
 
-//TODO: increment up the memNumBytes when I do all this macro expansion so we can accurately eval stuff
-// ld rd, L -> series of mov and shftli instructions to load 64-bit value
+// ld rd, L
 char *convertLdMacro(char *input)
 {
 	// int isZeros = 1; //means we're just starting off, leading bits are zero so can be discarded
-	char *buffer = malloc(sizeof(char)*50); // more than enough space
-	input += 3;				// to get rid of the tab + clr
+	char *buffer = malloc(sizeof(char) * 500);
+	buffer[0] = '\0';
+	input += 3; // to get rid of the tab + ld
 	long long value = 0;
 
 	char *token = strtok(input, delimiters); // the register
-	char *token2 = strtok(NULL, delimiters); // the data -- either a label or a literal\
+	char *token2 = strtok(NULL, delimiters); // the data -- either a label or a literal
 
-	if(token2[0] == ':'){
-		if(numRuns == 0){ //first pass, so inconsequential
+	if (token2[0] == ':')
+	{
+		if (numRuns == 0)
+		{ // first pass, so inconsequential
 			value = 0;
-		}else{
+		}
+		else
+		{
 			value = evaluateLabel(token2);
 		}
-	}else{
+	}
+	else
+	{
 		value = strtoll(token2, NULL, 10);
 	}
 
 	sprintf(buffer, "\n\txor %s, %s, %s", token, token, token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value >> 60) & 0xF);
+	sprintf(buffer + strlen(buffer), "\n\tshftli %s, 12", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value >> 48) & 0xFFF);
+	sprintf(buffer + strlen(buffer), "\n\tshftli %s, 12", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value >> 36) & 0xFFF);
+	sprintf(buffer + strlen(buffer), "\n\tshftli %s, 12", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value >> 24) & 0xFFF);
+	sprintf(buffer + strlen(buffer), "\n\tshftli %s, 12", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value >> 12) & 0xFFF);
+	sprintf(buffer + strlen(buffer), "\n\tshftli %s, 12", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi %s, %lld", token, (value & 0xFFF));
 
-	sprintf(buffer, "\n\taddi %s, %li", token, (value >> 60) & 0xF);
-	sprintf(buffer, "\n\tshftli %s, 12", token);
+	// one less than actual (11 instead of 12) since the data validation will add one //TODO: fix if we update the structure
+	if (type == 0)
+	{
+		memNumBytes += 11 * 8;
+	}
+	else if (type == 1)
+	{
+		memNumBytes += 11 * 4;
+	}
+	else
+	{
+		throwError("ERROR! Instruction called before .code or .data");
+	}
 
-	sprintf(buffer, "\n\taddi %s, %li", token, (value >> 48) & 0xFFF);
-	sprintf(buffer, "\n\tshftli %s, 12", token);
-
-	sprintf(buffer, "\n\taddi %s, %li", token, (value >> 36) & 0xFFF);
-	sprintf(buffer, "\n\tshftli %s, 12", token);
-
-	sprintf(buffer, "\n\taddi %s, %li", token, (value >> 24) & 0xFFF);
-	sprintf(buffer, "\n\tshftli %s, 12", token);
-
-	sprintf(buffer, "\n\taddi %s, %li", token, (value >> 12) & 0xFFF);
-	sprintf(buffer, "\n\tshftli %s, 12", token);
-
-	sprintf(buffer, "\n\taddi %s, %li", token, (value & 0xFFF));
-
-	memNumBytes += 11; //one less than actual since the data validation will add one //TODO: fix if we update the structure
-
-
-	// so shift over by 60 --> mask with 0xF
-	// then shift over by 12 --> mask with 0xFFF
-	// repeatedly shift over by 12 until all done
-
-	// TODO: parse rd and L from input
-	// If L is a label, resolve it first
-	// Return sequence: xor rd, rd, rd then mov/shftli chain
 	return buffer;
 }
 
-// in rd, rs -> priv rd, rs, r0, 0x3
+// in rd, rs
 char *convertInMacro(char *input)
 {
-	// TODO: parse rd, rs from input, return "priv rd, rs, r0, 0x3"
-	return NULL;
+	input += 3;								  // to get rid of the tab + in
+	char *buffer = malloc(sizeof(char) * 40); // more than enough space
+	buffer[0] = '\0';
+
+	char *token = strtok(input, delimiters); // rd
+	char *token2 = strtok(NULL, delimiters); // rs
+
+	sprintf(buffer, "\n\tpriv %s, %s, r0, %i", token, token2, 0x3);
+	return buffer;
 }
 
-// out rd, rs -> priv rd, rs, r0, 0x4
+// out rd, rs
 char *convertOutMacro(char *input)
 {
-	// TODO: parse rd, rs from input, return "priv rd, rs, r0, 0x4"
-	return NULL;
+	input += 4;								  // to get rid of the tab + out
+	char *buffer = malloc(sizeof(char) * 40); // more than enough space
+	buffer[0] = '\0';
+
+	char *token = strtok(input, delimiters); // rd
+	char *token2 = strtok(NULL, delimiters); // rs
+
+	sprintf(buffer, "\n\tpriv %s, %s, r0, %i", token, token2, 0x4);
+	return buffer;
 }
 
-// push rd -> subi r31, 8; mov (r31)(0), rd
+// push rd
 char *convertPushMacro(char *input)
 {
-	// TODO: parse rd from input
-	// Return two instructions: "subi r31, 8" and "mov (r31)(0), rd"
-	return NULL;
+	input += 5;								  // to get rid of the tab + push
+	char *buffer = malloc(sizeof(char) * 45); // more than enough space
+	buffer[0] = '\0';
+	char *token = strtok(input, delimiters); // rd
+
+	sprintf(buffer + strlen(buffer), "\n\tmov (r31)(-8), %s", token);
+	sprintf(buffer + strlen(buffer), "\n\tsubi r31, 8");
+
+	// NOTE: only adding 1 extra instruction since other is counted by the handleValidateNumArgs //todo: fix if change
+	if (type == 0)
+	{
+		memNumBytes += 8;
+	}
+	else if (type == 1)
+	{
+		memNumBytes += 4;
+	}
+	else
+	{
+		throwError("ERROR! Instruction called before .code or .data");
+	}
+
+	return buffer;
 }
 
-// pop rd -> mov rd, (r31)(0); addi r31, 8
+// pop rd
 char *convertPopMacro(char *input)
 {
-	// TODO: parse rd from input
-	// Return two instructions: "mov rd, (r31)(0)" and "addi r31, 8"
-	return NULL;
+	input += 4;								  // to get rid of the tab + pop
+	char *buffer = malloc(sizeof(char) * 45); // more than enough space
+	buffer[0] = '\0';
+	char *token = strtok(input, delimiters); // rd
+
+	sprintf(buffer + strlen(buffer), "\n\tmov %s, (r31)(0)", token);
+	sprintf(buffer + strlen(buffer), "\n\taddi r31, 8");
+
+	// NOTE: only adding 1 extra instruction since other is counted by the handleValidateNumArgs //todo: fix if change
+	if (type == 0)
+	{
+		memNumBytes += 8;
+	}
+	else if (type == 1)
+	{
+		memNumBytes += 4;
+	}
+	else
+	{
+		throwError("ERROR! Instruction called before .code or .data");
+	}
+
+	return buffer;
+}
+
+char *parseAndFormatArgs(char *inputString)
+{
+	char *outputLine = malloc(sizeof(char) * 50); // once again, plenty of space
+	outputLine[0] = '\0';
+	char delims[] = {" ,\t"};
+	char *token = strtok(inputString, delims);
+	sprintf(outputLine + strlen(outputLine), "\n\t%s ", token); // should be the first one -- the instruction
+	token = strtok(NULL, delims);
+	int isFirst = 1;
+	while (1)
+	{
+		if (token != NULL)
+		{
+			if (isFirst)
+			{
+				isFirst = 0;
+				sprintf(outputLine + strlen(outputLine), " %s", token);
+			}
+			else
+			{
+				sprintf(outputLine + strlen(outputLine), ", %s", token);
+			}
+		}
+		else
+		{
+			break;
+		}
+		token = strtok(NULL, delimiters);
+	}
+	//printf("Here's what I'm returning: %s\n", outputLine);
+	return outputLine;
+}
+
+void autoAdd(char *inputString)
+{
+	// 0 for data mode, 1 for int, -1 for unset
+	if (type == 0)
+	{
+		sprintf(data + strlen(data), parseAndFormatArgs(inputString));
+	}
+	else if (type == 1)
+	{
+		sprintf(code + strlen(code), parseAndFormatArgs(inputString));
+	}
+	else
+	{
+		throwError("ERROR! Once again called before type set somehow...");
+	}
+}
+
+void autoAddMacro(char *inputString)
+{
+	if (type == 0)
+	{
+		sprintf(data + strlen(data), inputString);
+	}
+	else if (type == 1)
+	{
+		sprintf(code + strlen(code), inputString);
+	}
+	else
+	{
+		throwError("ERROR! Once again called before type set somehow...");
+	}
 }
